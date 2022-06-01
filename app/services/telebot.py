e13b2d524a5f220851f7a66bae8fd0d1f022eac9
Kurdiansyah Lesmana <kurdilesmana@gmail.com>
2022-06-01 22:27:24 +0700

    initail app config

--- app/services/telebot.py
+++ app/services/telebot.py
@@ -58,20 +58,6 @@ async def on_chat_message(msg):
                     text='Location', request_location=True)],
             ])
             await bot.sendMessage(chat_id, 'Custom keyboard with various buttons', reply_markup=markup)
-        elif command == 'i':
-            markup = InlineKeyboardMarkup(inline_keyboard=[
-                [dict(text='Telegram URL', url='https://core.telegram.org/')],
-                [InlineKeyboardButton(
-                    text='Callback - show notification', callback_data='notification')],
-                [dict(text='Callback - show alert', callback_data='alert')],
-                [InlineKeyboardButton(
-                    text='Callback - edit message', callback_data='edit')],
-                [dict(text='Switch to using bot inline',
-                      switch_inline_query='initial query')],
-            ])
-
-            global message_with_inline_keyboard
-            message_with_inline_keyboard = await bot.sendMessage(chat_id, 'Inline keyboard with various buttons', reply_markup=markup)
         elif command == 'h':
             markup = ReplyKeyboardRemove()
             await bot.sendMessage(chat_id, 'Hide custom keyboard', reply_markup=markup)
